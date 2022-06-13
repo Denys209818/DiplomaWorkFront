2ba593ccb23a0b78db797b49c45954e12acc0d92
elik <elik@example.com>
2022-06-13 23:29:37 +0300

    Load div for footer

--- src/components/Default/ContentCardsMain.tsx
+++ src/components/Default/ContentCardsMain.tsx
@@ -27,13 +27,6 @@ const stylesForBlock: React.CSSProperties = {
   marginTop: '2em',
 };
 
-const stylesForJumbotron: React.CSSProperties = {
-  marginTop: '4em',
-  backgroundColor: '#BDD4D9',
-  width: '100%',
-  maxWidth: '100%',
-};
-
 const ContentCardsMain: React.FC = () => {
   return (<>
     <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
@@ -76,18 +69,6 @@ const ContentCardsMain: React.FC = () => {
           </Grid>
         ))}
       </Grid>
-    </Container>
-
-
-    <Container style={stylesForJumbotron} component="div" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-      <Typography variant="h2" component="h1" gutterBottom>
-        Sticky footer
-      </Typography>
-      <Typography variant="h5" component="h2" gutterBottom>
-        {'Pin a footer to the bottom of the viewport.'}
-        {'The footer will move as the main element of the page grows.'}
-      </Typography>
-      <Typography variant="body1">Sticky footer placeholder.</Typography>
     </Container>
   </>
   );
