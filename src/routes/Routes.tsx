5e17597a4bff238307fc74db4edb7214010f4731
elik <elik@example.com>
2022-06-14 12:34:44 +0300

    Change structure

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { RouteObjectType } from "./types";
 
-const Main = React.lazy(() => import("../components/Default/Main"));
-const Login = React.lazy(() => import("../components/Default/Login"));
+const Main = React.lazy(() => import("../components/Default/Main/Main"));
+const Login = React.lazy(() => import("../components/Default/Login/Login"));
 
 
 export const DefaultRoutes : Array<RouteObjectType>  = 
