9f897b40281f745221a177eb6407e02d2637246e
elik <elik@example.com>
2022-08-22 22:48:24 +0300

    testtat

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,10 +5,6 @@ import { RootReducer, rootReducer } from './reducers/rootReducer';
 import { composeWithDevTools } from "redux-devtools-extension";
 import { useDispatch } from 'react-redux';
 
-import { legacy_createStore as createStore, applyMiddleware} from 'redux'
-import thunk from "redux-thunk";
-import { rootReducer } from './reducers/rootReducer';
-import { composeWithDevTools } from "redux-devtools-extension";
 
 
 var middleware = [thunk];
