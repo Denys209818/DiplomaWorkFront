00b5d747dee25f25f2d57c78e9c0ce6a3e86a525
Denys209818 <59306120+Denys209818@users.noreply.github.com>
2022-09-11 14:56:51 +0300

    Denys (#38)
    * new branch
    
    * register
    
    * load chat for users and groups
    
    * axioxdreduser friend
    
    * add profile changes
    
    * add profile and loader
    
    * repair axiosFile
    
    * add changes to main page
    
    * Friend action
    
    * add page post
    
    * add fix bags with router
    
    * add fix bag changes
    
    * add register fix
    
    Co-authored-by: Maxim <rqadchenkom@gmail.com>

--- src/components/Auth/Register/yupValidation.tsx
+++ src/components/Auth/Register/yupValidation.tsx
@@ -1,15 +1,13 @@
 import * as Yup from "yup";
 
 export default Yup.object({
-    firstName: Yup.string().required('Поле обов\'язкове для заповнення!')
-    .matches(/^[A-Za-z]*$/, 'Введіть дійсне ім\'я').max(25),
-    secondName: Yup.string().required('Поле обов\'язкове для заповнення!')
-    .matches(/^[A-Za-z]*$/, 'Введіть дійсне прізвище').max(25),
+    firstName: Yup.string().required('Поле обов\'язкове для заповнення!').max(25),
+    secondName: Yup.string().required('Поле обов\'язкове для заповнення!').max(25),
     phone: Yup.string().required('Поле обов\'язкове для заповнення!'),
     email: Yup.string().required('Поле обов\'язкове для заповнення!')
     .email('Поле електронної введено не вірно!'),
     password: Yup.string().required('Поле обов\'язкове для заповнення!')
     .min(6, 'Мінімальна кількість значень - 6'),
-    passwordConfirmation: Yup.string().required('Поле обов\'язкове для заповнення!')
+    confirmPassword: Yup.string().required('Поле обов\'язкове для заповнення!')
     .oneOf([Yup.ref('password'), null], 'Пароль повинен збігатися')
 }); 
